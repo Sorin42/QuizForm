571f10f7de153e361ea8a7df469cf81e717415ca
Sorin42 <104473139+Sorin42@users.noreply.github.com>
2022-08-18 17:18:42 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var canvas, backgroundImage;
+
+var questions;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  question = new Question()
+  question.display();
+//call the start() method inside the quiz class
+
+}
+
+
+function draw(){
+  background("pink");
+
+}
